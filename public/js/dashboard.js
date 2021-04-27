490c3512622954294f56aadb9db5665bcf959598
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-27 11:33:11 -0500

    Added click row to update.

--- public/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-// let rowEl = document.querySelector(".my-post-text");
-
-// function rowHandler(event) {
-
-//   let rowId = rowEl.getAttribute("data-id");
-//   alert("The id: "  + rowId);
-// }
-
-// rowEl.addEventListener("click", rowHandler);
