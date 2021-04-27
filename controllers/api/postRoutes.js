aac65fc9d022ee7386d3a38f393186d466d3f258
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-27 01:49:09 -0500

    Added comments.handlebars.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -18,6 +18,28 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('update', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.put('/:id', withAuth, async (req, res) => {
   try {
     console.log("\n\nPut\n\n");
