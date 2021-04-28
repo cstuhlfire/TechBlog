2a840203fb5910babd04bf468bbe5b0071d07b7d
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-04-27 19:10:33 -0500

    Added GET path to comments.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -98,6 +98,28 @@ router.get('/update/:id', async (req, res) => {
   }
 });
 
+router.get('/comments/:id', async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          //attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('comments', {
+      ...post
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to home "/"
   if (req.session.logged_in) {
